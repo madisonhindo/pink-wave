78aeb71ab89a29ccd384532835dda41d45114281
madisonhindo <43414400+madisonhindo@users.noreply.github.com>
2018-12-05 22:38:53 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var mymap = L.map('winMap').setView([37.172, -89.494], 3.40);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/madisonhindo/cjp9wng9444ah2ssga0mkkq04/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFkaXNvbmhpbmRvIiwiYSI6ImNqcDFnMGNhOTNmMDAzcWt3ZzE1N25sNWoifQ.y96dGGE5ySq9C-maEeI9tg', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18
+}).addTo(mymap);
+
+for ( let i = 0; i < places.length; i++) {
+  L.marker( [ places[i].latitude, places[i].longitude ] )
+  .bindPopup( '<h3>' + places[i].placeName + '</h3>' + '<p>' +
+      places[i].descrip + '</p>')
+  .addTo(mymap);
+}
+
+$(document).ready(function() {
+  function hideAll() {
+    $('#ocasioText').hide();
+    $('#omarText').hide();
+    $('#pressleyText').hide();
+    $('#sinemaText').hide();
+}
+
+  hideAll();
+
+$('#womenPhotos').click(function() {
+    hideAll();
+    switch ($(this).attr("id")) {
+      case 'ocasiocortez':
+        $('#ocasioText').show();
+        break;
+      case 'omar':
+        $('#omarText').show();
+        break;
+      case 'pressley':
+        $('#pressleyText').show();
+        break;
+      case 'sinema':
+        $('#sinemaText').show();
+        break;
+    }
+  });
+  });
